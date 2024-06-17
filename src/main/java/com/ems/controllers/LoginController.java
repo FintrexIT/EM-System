9366a3515256cf66b3154124fcdbfb751be550e1
Chirantha Marasinghe <chirantha.20210210@iit.ac.lk>
2024-06-17 15:25:14 +0530

    UAT Deploy commit

--- src/main/java/com/ems/controllers/LoginController.java
+++ src/main/java/com/ems/controllers/LoginController.java
@@ -19,61 +19,59 @@ public class LoginController {
     @Autowired
     LoginService servr;
 
+//    @PostMapping("/login")
+//    public String checkLogin(@RequestParam String username, @RequestParam String password, HttpSession session) {
+//        if (password.trim().equals("")) {
+//            return "error";
+//        }
+//        Users user = servr.checkLogin(username);
+//
+//        if (user == null) {
+//            return "error";
+//        } else {
+//
+//            Hashtable props = new Hashtable();
+//            props.put(Context.SECURITY_PRINCIPAL, username + "@fintrexfinance.com");
+//            props.put(Context.SECURITY_CREDENTIALS, password);
+//            props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+//            props.put(Context.PROVIDER_URL, "ldap://ad1.fintrex.lk:389");
+//
+//            try {
+//                InitialLdapContext c = new InitialLdapContext(props, null);
+//                session.setAttribute("uid", user.getId());
+//                session.setAttribute("branch", user.getBranch());
+//                session.setAttribute("username", username);
+//                session.setAttribute("usertype", user.getUsertype());
+//
+//                return "ok";
+//
+//            } catch (javax.naming.CommunicationException e) {
+//                e.printStackTrace();
+//                return "error";
+//            } catch (NamingException e) {
+//                e.printStackTrace();
+//                return "error";
+//            }
+//
+//        }
+//    }
     @PostMapping("/login")
-    public String checkLogin(@RequestParam String username, @RequestParam String password, HttpSession session) {
-        if (password.trim().equals("")) {
-            return "error";
-        }
+    public String checkLogin(@RequestParam String username, HttpSession session) {
+
         Users user = servr.checkLogin(username);
 
         if (user == null) {
             return "error";
         } else {
 
-            Hashtable props = new Hashtable();
-            props.put(Context.SECURITY_PRINCIPAL, username + "@fintrexfinance.com");
-            props.put(Context.SECURITY_CREDENTIALS, password);
-            props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-            props.put(Context.PROVIDER_URL, "ldap://ad1.fintrex.lk:389");
+            session.setAttribute("uid", user.getId());
+            session.setAttribute("branch", user.getBranch());
+            session.setAttribute("username", username);
+            session.setAttribute("usertype", user.getUsertype());
 
-            try {
-                InitialLdapContext c = new InitialLdapContext(props, null);
-                session.setAttribute("uid", user.getId());
-                session.setAttribute("branch", user.getBranch());
-                session.setAttribute("username", username);
-                session.setAttribute("usertype", user.getUsertype());
-
-                return "ok";
-
-            } catch (javax.naming.CommunicationException e) {
-                e.printStackTrace();
-                return "error";
-            } catch (NamingException e) {
-                e.printStackTrace();
-                return "error";
-            }
+            return "ok";
 
         }
     }
 
-//    @PostMapping("/login")
-//    public String checkLogin(@RequestParam String username, HttpSession session) {
-//
-//        User user = servr.checkLogin(username);
-//
-//        if (user == null) {
-//            return "error";
-//        } else {
-//
-//            session.setAttribute("uid", user.getId());
-//            session.setAttribute("username", username);
-////            session.setAttribute("type", user.getUserType().getId());
-////            String dashboard = servr.getDashboard(user.getUserType().getId());
-////            session.setAttribute("dashboard", dashboard);
-//
-//            return "ok";
-//
-//        }
-//    }
-//
 }
